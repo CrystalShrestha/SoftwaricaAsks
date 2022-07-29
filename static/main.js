618b995e2aa270932a28aa14d87732d0bd4e1640
CrystalShrestha <crystalstha91@gmail.com>
2022-07-29 21:07:15 +0545

    Final Crystal

--- static/main.js
+++ static/main.js
@@ -6,6 +6,42 @@ button.addEventListener('click', (event) => {
 })
 
 
+/*Questions Section */
+
+let questions_img=[
+    'https://cdn.sstatic.net/Img/home/illo-feats-ask.svg?v=b6cd07f0765a',
+    'https://cdn.sstatic.net/Img/home/illo-feats-vote.svg?v=9d2eb0efdc17',
+    'https://cdn.sstatic.net/Img/home/illo-feats-answer.svg?v=b637b99bc32a',
+    'https://cdn.sstatic.net/Img/home/illo-feats-tags.svg?v=0655cbe6bffa',
+    'https://cdn.sstatic.net/Img/home/illo-feats-accept.svg?v=f2be4b8dfdac',
+    'https://cdn.sstatic.net/Img/home/illo-feats-recognize.svg?v=4f011d7173e8'
+]
+
+var questionsBody=document.querySelector('.questions-body');
+var question_items=questionsBody.querySelectorAll('.questions-body-item')
+var question_content_img=questionsBody.querySelector('.question-item-content-img')
+var question_item_isSelected=false;
+
+function resetActive(){
+    question_items.forEach(item=>{
+        item.classList.remove("active");
+    })
+}
+function getDataId(item){
+    return item.getAttribute('data-id');
+}
+function changeImage(id){
+    question_content_img.src=questions_img[id];
+}
+
+question_items.forEach(item=>{
+    item.addEventListener('click',function (event){
+        question_item_isSelected=true;
+        resetActive();
+        this.classList.add('active')
+        changeImage(getDataId(item))
+    })
+})
 
 /*Questions Animation*/
 var index=0;
